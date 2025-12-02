29837b01d1dadf075e5936a4d23d094e530e880e
Bhushan Acharekar <bhushanacharekar1725@gmail.com>
2025-12-02 17:17:47 +0530

    Revert "feat(auth): add mongodb service in index"

--- backend/auth/src/index.js
+++ backend/auth/src/index.js
@@ -1,16 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';
-import { connectDB } from '../config/db';
 
 dotenv.config();
 const app = express();
 const port = 5001;
 
-app.use(cors());
 app.use(express.json());
-
-connectDB();
 
 app.get('/health', (req, res) => {
   res.json({ status: 'auth-service-ok' });
